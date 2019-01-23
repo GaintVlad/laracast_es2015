df64290e69f376527194eac5b1ecfb710473c7be
GaintVlad <sadkoff@tut.by>
2019-01-23 17:21:26 +0300

    lisson-16, iterators

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 function *range(start, end) {
     while (start <= end) {
         yield start;
+        start ++;
     }
 }
 
-iterator =
+let iterator = range(1, 5);
+
+console.log(iterator.next(), iterator.next(), iterator.next());
+
+
+//*****************************//
+let iterator2 = range(1, 5);
+
+for (let i of iterator2) {
+    console.log(i);
+}
+
+//***************************//
+console.log([...range(1,5)])
