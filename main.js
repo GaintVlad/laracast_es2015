18f7f7d5a95a06ef15679aaf807ce2756df3d1a4
GaintVlad <sadkoff@tut.by>
2019-01-23 17:00:24 +0300

    node_mod folder was added to gitignore

--- main.js
+++ main.js
@@ -1,31 +1,7 @@
-class TaskCollection {
-    constructor(task = []) {
-        this.task = task;
-    }
-    log() {
-        this.task.forEach((task, index) => console.log(task, index));
-    }
-}
-class Task {}
-
-new TaskCollection([new Task(),new Task(), new Task()]).log()
-
-//return keyword
-let names = ['Matt', 'Jeffry', 'Bill'];
-
-names =  names.map(name => `${name} +  is coll`);
-console.log(names)
-
-const obj = {
-    defaultDiscount() {return 0.10},
-    getDiscount(value, discount = this.defaultDiscount()) {
-        return value - (value * discount);
-    },
-    init(discount) {
-        alert(this.getDiscount(100, discount))
+function *range(start, end) {
+    while (start <= end) {
+        yield start;
     }
 }
 
-obj.init()
-
-
+iterator =
